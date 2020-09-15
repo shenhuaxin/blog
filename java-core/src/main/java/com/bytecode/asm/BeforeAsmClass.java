f0cef0aa678820fd786cb2d26bb97a5a914b8317
shenhuaxin <shenhuaxin@extracme.com>
2020-09-15 13:04:14 +0800

    add a empty method

--- java-core/src/main/java/com/bytecode/asm/BeforeAsmClass.java
+++ java-core/src/main/java/com/bytecode/asm/BeforeAsmClass.java
@@ -4,10 +4,12 @@ public class BeforeAsmClass {
     public int a = 0;
     public int b = 1;
 
-    public String age = "10";
     public void test01() {
     }
 
     public void test02() {
+    }
+
+    public static void main(String[] args) {
     }
 }
--- java-core/src/main/java/com/bytecode/asm/ChangeClassTest.java
+++ java-core/src/main/java/com/bytecode/asm/ChangeClassTest.java
@@ -1,11 +1,13 @@
 package com.bytecode.asm;
 
 import org.objectweb.asm.*;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.MethodNode;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-import static org.objectweb.asm.Opcodes.ASM5;
+import static org.objectweb.asm.Opcodes.*;
 
 public class ChangeClassTest {
 
@@ -13,8 +15,39 @@ public class ChangeClassTest {
     public static void main(String[] args) throws IOException {
 
         addField();
+        addMethodByCoreApi();
     }
 
+
+    public static void addMethodByTreeApi() throws IOException {
+        ClassReader classReader = new ClassReader(BeforeAsmClass.class.getName());
+        ClassWriter classWriter = new ClassWriter(0);
+        ClassNode classNode = new ClassNode();
+        classReader.accept(classNode, ClassReader.SKIP_CODE);
+        MethodNode methodNode = new MethodNode(ACC_PUBLIC, "name", "(Ljava/lang.String;)V", null, null);
+        classNode.methods.add(methodNode);
+
+        classNode.accept(classWriter);
+        write(BeforeAsmClass.class, classWriter.toByteArray());
+    }
+
+    public static void addMethodByCoreApi() throws IOException {
+        ClassReader classReader = new ClassReader(BeforeAsmClass.class.getName());
+        ClassWriter classWriter = new ClassWriter(0);
+        ClassVisitor classVisitor = new ClassVisitor(ASM8, classWriter) {
+            @Override
+            public void visitEnd() {
+                super.visitEnd();
+                MethodVisitor method = cv.visitMethod(ACC_PUBLIC, "name", "(Ljava/lang.String;)V", null, null);
+                if (method != null) {
+                    method.visitEnd();
+                }
+
+            }
+        };
+        classReader.accept(classVisitor, ClassReader.SKIP_CODE);
+        write(BeforeAsmClass.class, classWriter.toByteArray());
+    }
 
     public static void addField() throws IOException {
         ClassReader classReader = new ClassReader(BeforeAsmClass.class.getName());
@@ -23,35 +56,23 @@ public class ChangeClassTest {
             @Override
             public void visitEnd() {
                 super.visitEnd();
-                FieldVisitor name = cv.visitField(Opcodes.ACC_PUBLIC, "name", "Ljava/lang/String;", null, "shenhuaxin");
+                FieldVisitor name = cv.visitField(Opcodes.ACC_PUBLIC, "name", "Ljava/lang/String;", null, null);
                 if (name != null) {
                     name.visitEnd();
                 }
             }
         };
         classReader.accept(cv, ClassReader.SKIP_CODE | ClassReader.SKIP_DEBUG);
-
-        String path = ClassLoader.getSystemResource(BeforeAsmClass.class.getName().replace('.', '/')+".class").getPath();
-
-        byte[] bytes = classWriter.toByteArray();
-
-        FileOutputStream fileOutputStream = new FileOutputStream(path);
-        fileOutputStream.write(bytes);
+        write(BeforeAsmClass.class, classWriter.toByteArray());
     }
 
+    public static <T> void write(Class<T> clazz, byte[] bytes) {
+        String path = ClassLoader.getSystemResource(clazz.getName().replace('.', '/') + ".class").getPath();
 
-    public static void addMethod() throws IOException {
-        ClassReader classReader = new ClassReader(BeforeAsmClass.class.getName());
-        ClassWriter classWriter = new ClassWriter(0);
-        ClassVisitor classVisitor = new ClassVisitor(ASM5, classWriter) {
-            @Override
-            public void visitEnd() {
-                super.visitEnd();
-                FieldVisitor name = classWriter.visitField(Opcodes.ACC_PUBLIC, "name", "Ljava/lang/String", null, null);
-                if (name != null) {
-                    name.visitEnd();
-                }
-            }
-        };
+        try (FileOutputStream fileOutputStream = new FileOutputStream(path);) {
+            fileOutputStream.write(bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
